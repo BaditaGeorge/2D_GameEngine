7c33d2a1281e7b7897f8cf495836693ccaf87d9e
BaditaGeorge <baditageorge@yahoo.com>
2020-05-14 14:48:41 +0300

    diagrams and update on ember ts classes

--- app/controllers/yourapp.ts
+++ app/controllers/yourapp.ts
@@ -3,7 +3,11 @@ import Controller from '@ember/controller';
 export default class Yourapp extends Controller.extend({
   // anything which *must* be merged to prototype here
 }) {
-  config_arr:string = JSON.stringify([{"type":"circle","cx":"200","cy":"100","r":"50","fill":"red"},{"type":"circle","r":"100","cx":"300","cy":"300"},{"type":"rect","w":"50","h":"50","x":"10","y":"10"},{"type":"polygon","points":[150,0,75,200,225,200],"fill":"blue"}]);
+  config_arr:string = JSON.stringify([{"type":"circle","cx":"200","cy":"100","r":"50","fill":"green"},
+  {"type":"circle","r":"100","cx":"300","cy":"300"},
+  {"type":"rect","w":"50","h":"50","x":"10","y":"10"},
+  {"type":"polygon","points":[150,0,75,200,225,200],"fill":"blue"}]);
+  elementsToRender:Array<string> = ['render/group'];
   constructor(){
     super(...arguments);
   }
